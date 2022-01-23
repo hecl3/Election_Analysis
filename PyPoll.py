b1b9bec7536f97c3735df6853e182d3dbf57df33
hecl3 <94264746+hecl3@users.noreply.github.com>
2022-01-23 00:40:47 -0600

    Add files via upload

---
+++ PyPoll.py
@@ -0,0 +1,32 @@
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do: read and analyze the data here.
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+# To do: perform analysis.
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the open() function with the "w" mode we will write data to the file.
+open(file_to_save, "w")
+# Write three counties to the file.
+text_file.write("Arapahoe\nDenver\nJefferson")
+# Close the file.
+election_data.close()
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on the popular vote.
